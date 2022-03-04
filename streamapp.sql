fcce305b9a8cb1ab6eb021f395b76a7543a510d0
ruhina3107 <81763796+ruhina3107@users.noreply.github.com>
2022-03-04 20:43:08 +0400

    SQL update

--- streamapp.sql
+++ streamapp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 03, 2022 at 02:55 PM
+-- Generation Time: Mar 04, 2022 at 05:40 PM
 -- Server version: 10.4.18-MariaDB
 -- PHP Version: 8.0.3
 
@@ -31,18 +31,23 @@ CREATE TABLE `users` (
   `Id` int(255) NOT NULL,
   `userName` varchar(255) DEFAULT NULL,
   `userMail` varchar(255) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL
+  `password` varchar(255) DEFAULT NULL,
+  `userMobile` varchar(255) DEFAULT NULL,
+  `isAdmin` int(11) DEFAULT 0,
+  `createdAt` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`Id`, `userName`, `userMail`, `password`) VALUES
-(1, 'ruhina', 'ruhina', 'ruhina'),
-(2, NULL, 'user2', 'user2'),
-(3, NULL, 'user3', 'user3'),
-(4, NULL, 'user4', 'user4');
+INSERT INTO `users` (`Id`, `userName`, `userMail`, `password`, `userMobile`, `isAdmin`, `createdAt`) VALUES
+(1, 'ruhina', 'ruhina', 'ruhina', '+916788999000', 0, '2022-03-04 00:00:00'),
+(2, 'admin', 'admin', 'admin', '+789656778', 1, '2022-03-04 00:00:00'),
+(3, 'user3', 'user3', 'user3', '+98907654', 0, '2022-03-04 00:00:00'),
+(4, 'user4', 'user4', 'user4', '+45678900', 0, '2022-03-04 00:00:00'),
+(5, '', 'zain@gmail.com', 'zain', '8756789', 0, '2022-03-04 00:00:00'),
+(6, 'zayed', 'zayed@gmail.com', 'zayed', '97234561', 0, '2022-03-04 00:00:00');
 
 --
 -- Indexes for dumped tables
@@ -62,7 +67,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `Id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `Id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
